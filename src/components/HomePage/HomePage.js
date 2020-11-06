f9b5c669ae7afd21491d278d6cbbdf55e50a26e6
Matthew Harris <matthewharrisak@gmail.com>
2020-11-06 07:51:50 -0600

    added axios-get to HomePage component and logged out the response.

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,13 +1,39 @@
+import Axios from 'axios';
 import React, { Component } from 'react';
 import {HashRouter as Router, Route} from 'react-router-dom';
+import axios from 'axios';
 
 class HomePage extends Component {
 
+    state = {
+        movieObject: []
+    };
 
+    getMovies = () => {
+        console.log('in get movies!');
+        axios({
+            method: 'GET',
+            url: '/api/genre'
+        }).then((response) => {
+            console.log(response.data);
+            this.setState({
+                movieObject: response.data                
+            });
+            console.log(this.state , 'from GetMOvies state');
+            
+        }).catch((error) => {
+            console.log(error);
+        });
+    }
+
+    componentDidMount = () => {
+        this.getMovies();
+    }
   render() {
     return (
       <div className="App">
         <h1>Home Goes HERE</h1>
+
         <button>Details</button>
       </div>
     );
