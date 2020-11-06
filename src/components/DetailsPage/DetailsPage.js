31093fe25477d3d81149902e081c631f9a677a10
Matthew Harris <matthewharrisak@gmail.com>
2020-11-05 20:44:42 -0600

    add detials page componet

---
+++ src/components/DetailsPage/DetailsPage.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+// import {HashRouter as Router, Route} from 'react-router-dom';
+
+class DetailsPage extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Home Goes HERE</h1>
+   
+      </div>
+    );
+  }
+}
+
+export default DetailsPage;
