61fcc5a237e510324f853d2f18a0ebd3998ddbc4
Matthew Harris <matthewharrisak@gmail.com>
2020-11-07 12:12:38 -0600

    created MovieItem component and moved poster display to that component

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,31 +1,17 @@
-import Axios from 'axios';
 import React, { Component } from 'react';
 import {HashRouter as Router, Route} from 'react-router-dom';
-import axios from 'axios';
 import { connect } from 'react-redux';
+import MovieItem from '../MovieItem/MovieItem';
 
 class HomePage extends Component {
 
-    // fires off funtions on pageload! 
-    componentDidMount = () => {
-     console.log(this.props.reduxState);
-     this.props.dispatch({ type: 'GOT_MOVIES'});
-
-     }
-
+ 
   
 
   render() {
     return (
-      <div className="App">
-        <h1>Home Goes HERE</h1>
-        {this.props.reduxState.movies.map((poster) =>{
-        return  <div className='posterDiv'> 
-                <button className='posterButton'><img  src={poster.poster}></img> <h4> {poster.title} </h4> 
-                </button> 
-                </div>
-        })}
-        {/* {JSON.stringify(this.props.reduxState.movies)} */}
+      <div className="homePage">
+          <MovieItem/>
       </div>
     );
   }
