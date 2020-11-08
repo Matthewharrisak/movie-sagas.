195516f715e5d9d5ac44384b05ccc714da056532
Matthew Harris <matthewharrisak@gmail.com>
2020-11-07 20:46:05 -0600

    added handle change function to Addmovies component

--- src/components/AddMovies/AddMovies.js
+++ src/components/AddMovies/AddMovies.js
@@ -3,10 +3,18 @@ import {HashRouter as Router, Route} from 'react-router-dom';
 
 class AddMovies extends Component {
 
+  handleChange = (event) => {
+    console.log(event , 'waht is logging?');
+    
+  //   this.setState({
+  //   supported: event.target.value
+  // });
+}
+
   render() {
     return (
       <div className="App">
-       <form>
+       <form onSubmit={this.handleChange} >
        <input type="text" id="newMovieTitle"/>
        <button> SUBMIT NEW MOVIE </button>
        </form>
