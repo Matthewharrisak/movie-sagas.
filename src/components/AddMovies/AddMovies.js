d4981653a55697f031bec708d4c640b818dc779d
Matthew Harris <matthewharrisak@gmail.com>
2020-11-08 10:54:43 -0600

    hooked up input form on AddMovies component

--- src/components/AddMovies/AddMovies.js
+++ src/components/AddMovies/AddMovies.js
@@ -3,25 +3,49 @@ import {HashRouter as Router, Route} from 'react-router-dom';
 
 class AddMovies extends Component {
 
-  handleChange = (event) => {
-    console.log(event , 'waht is logging?');
+  state={ 
+    newMovie:{
+    title: '',
+    poster: '',
+    description: '',
+  }}
+
+  handleChange= (keyname, event) => {
+    this.setState({
+      newMovie:{
+        ...this.state.newMovie,
+        [keyname]: event.target.value,
+    }});
+  }
+
+  addMovie= (event) => {
+    event.preventDefault();
+
+    console.log('were logging state' , this.state);
     
-  //   this.setState({
-  //   supported: event.target.value
-  // });
-}
+  }
 
   render() {
     return (
       <div className="App">
-       <form onSubmit={this.handleChange} >
-       <input type="text" id="newMovieTitle"/>
+       <form onSubmit={this.addMovie}>
+       <input value={this.state.newMovie.title} onChange={(event) =>this.handleChange( 'title' , event)} type="text" id="newMovieTitle"/>
+       <input value={this.state.newMovie.poster} onChange={(event) => this.handleChange('poster' , event)} type="text" id="newDescription"/>
+       <input value={this.state.newMovie.description} onChange={(event) => this.handleChange('description' , event)} type="text" id="newPoster"/>
+      
        <button> SUBMIT NEW MOVIE </button>
        </form>
-   
+      <h1> {this.state.newMovie.newPoster} </h1>
+      <h1> {this.state.newMovie.newTitle} </h1>
+      <h1> {this.state.newMovie.newDescription} </h1>
+
       </div>
     );
   }
 }
 
+
+// title: event.target.value,
+// poster: event.target.value,
+// description: event.target.value,
 export default AddMovies;
